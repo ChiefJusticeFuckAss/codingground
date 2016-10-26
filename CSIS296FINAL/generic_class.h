d841e16e31098f2096047434d562761e4055984b
ChiefJusticeFuckAss <jake.palmer31@gmail.com>
2016-10-25 18:14:46 -0700

    Update generic_class.h

--- CSIS296FINAL/generic_class.h
+++ CSIS296FINAL/generic_class.h
@@ -3,14 +3,25 @@
 #ifndef __GENERIC_CLASS_H__
 #define __GENERIC_CLASS_H__
 
+
 class myclass_gen {
     private:
-        int value;
-        string payload;
+        int     value_;
+        string  payload_;
     public:
-        myclass_gen();
-        ~myclass_gen();
-        
+        myclass_gen()   {}
+        ~myclass_gen()  {}
+        virtual void primary_function() = 0;
+};
+
+class myclass_player : public myclass_gen {
+    private:
+        string name_    = DEFAULT;
+    public:
+        myclass_player()    {}
+        myclass_player(string* name);
+        ~myclass_player();
+        virtual void primary_function();
 };
 
 #endif
