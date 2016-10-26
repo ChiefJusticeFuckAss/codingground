9eab8c0233068075e6a008d5d04536d1d3eb3cae
ChiefJusticeFuckAss <jake.palmer31@gmail.com>
2016-10-25 18:14:42 -0700

    Update class_definitions.cpp

--- CSIS296FINAL/class_definitions.cpp
+++ CSIS296FINAL/class_definitions.cpp
@@ -1,4 +1,22 @@
-#include "generic_class"
+
 #include <string>
 #include <memory>
+#define DEFAULT "JAKE"
+#include "generic_class"
 
+using namespace std;
+
+myclass_player::myclass_player()
+{
+  unique_ptr<string> ptr(new string());
+  cout  << "\nPlease enter your name\n>>>";
+  cin   >> *ptr;
+  this  -> name_ = *ptr;
+  cout  << "\nWelcome, "
+        << this -> name_
+        << endl;
+}
+
+myclass_player::myclass_player(string* name) : name_(*name)
+{
+}
