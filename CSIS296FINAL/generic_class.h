177166e6793b0d2e717a953a589b74bb7fbcfae5
ChiefJusticeFuckAss <jake.palmer31@gmail.com>
2016-10-25 17:50:35 -0700

    Created by Coding Ground Client

---
+++ CSIS296FINAL/generic_class.h
@@ -0,0 +1,16 @@
+
+
+#ifndef __GENERIC_CLASS_H__
+#define __GENERIC_CLASS_H__
+
+class myclass_gen {
+    private:
+        int value;
+        string payload;
+    public:
+        myclass_gen();
+        ~myclass_gen();
+        
+};
+
+#endif
