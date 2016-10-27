36230c91952be08a4c2c3506cb1a0b338fe3a72a
ChiefJusticeFuckAss <jake.palmer31@gmail.com>
2016-10-26 19:59:04 -0700

    Update main.cpp

--- CSIS296FINAL/main.cpp
+++ CSIS296FINAL/main.cpp
@@ -4,8 +4,18 @@ using namespace std;
 
 int main()
 {
-    
+   unique_ptr<int>      still_playing   (new int(1));
+   unique_ptr<string>   user_choice     (new string());
    
+   while(*still_playing ==  1)
+   {
+       have_a_game();
+       
+       cout    << "\nWould you like to play again? (Y/N)\n>>>  ";
+       cin     >> *user_choice;
+       
+       (*user_choice == "y" || *user_choice == "Y") ? (cout << "Okay, another round then.") : (*still_playing = 0);
+   }
    return 0;
 }
 
